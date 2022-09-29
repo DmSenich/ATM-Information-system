c5c118b7e5110064d465a73bb4100dc712675a40
DmSenich <dsenichkin@yandex.ru>
2022-09-29 10:05:01 +0300

    Новые классы для операций между людьми

--- ATM-information-system/ATM/ATM.cs
+++ ATM-information-system/ATM/ATM.cs
@@ -8,5 +8,35 @@ namespace ATM
 {
     internal class ATM
     {
+        string name;
+        int balance;
+
+        private ATM() { }
+
+        public ATM(string name, int balance)
+        {
+            this.name = name;
+            this.balance = balance;
+        }
+
+        string Name { get { return name; } }
+        public int Balance { get { return balance; } }
+
+        public void AddMoney(int p)
+        {
+            if(p < 0)
+            {
+                return;
+            }
+            balance += p;
+        }
+        public void RemMoney(int p)
+        {
+            if(p < 0)
+            {
+                return ;
+            }
+            balance -= p;
+        }
     }
 }
---
+++ ATM-information-system/ATM/Operation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    internal class Operation
+    {
+        string addressee, recipient;
+        int moneyTransfer;
+
+        private Operation() { }
+        public Operation(string recipient, string addressee, int moneyTransfer)
+        {
+            this.recipient = recipient;
+            this.addressee = addressee;
+            this.moneyTransfer = moneyTransfer;
+        }
+    }
+}
