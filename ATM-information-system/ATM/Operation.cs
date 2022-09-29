65e358c7979dcae5f81af57ca13cb525ec4c0400
DmSenich <dsenichkin@yandex.ru>
2022-09-29 11:30:57 +0300

    Реализация управления xml-файлом и изменение класса Operation

--- ATM-information-system/ATM/Operation.cs
+++ ATM-information-system/ATM/Operation.cs
@@ -8,15 +8,35 @@ namespace ATM
 {
     internal class Operation
     {
-        string addressee, recipient;
-        int moneyTransfer;
+        string person, numBank;
+        decimal moneyTransfer;
+        bool doing;
 
         private Operation() { }
-        public Operation(string recipient, string addressee, int moneyTransfer)
+        public Operation(string numBank, string person, decimal moneyTransfer, bool d)
         {
-            this.recipient = recipient;
-            this.addressee = addressee;
+            this.numBank = numBank;
+            this.person = person;
             this.moneyTransfer = moneyTransfer;
+            this.doing = d;
+        }
+        public string Person { get { return person; } }
+        public string NumBank { get { return numBank; } }
+        public decimal MoneyTransfer { get { return moneyTransfer; } }
+        public bool Doing { get { return doing; } }
+
+        public string DoingSt()
+        {
+            string st;
+            if (Doing)
+            {
+                st = "Внести в банкомант";
+            }
+            else
+            {
+                st = "Снять наличные";
+            }
+            return st;
         }
     }
 }
